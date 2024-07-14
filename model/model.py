e5f158d8e5f4f407d5d0529df5f2fd35d8495187
al771 <58562943+al771@users.noreply.github.com>
2024-07-15 02:40:49 +0300

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,37 +1,52 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, mean_absolute_error
 import joblib
+import numpy as np
+import lightgbm as lgb
 
 
 # Загрузка данных
-
 X_train = pd.read_csv('X_train.csv')
 y_train = pd.read_csv('y_train.csv')
 X_test = pd.read_csv('X_test.csv')
 y_test = pd.read_csv('y_test.csv')
 
-
-# Преобразование столбцов, которые должны быть числовыми
-#X_train = X_train.apply(lambda x: x.str.replace(' ', '').astype(float) if x.dtype == 'object' else x)
-
-#X_test = X_test.apply(lambda x: x.str.replace(' ', '').astype(float) if x.dtype == 'object' else x)
-
 # Создание модели
 
-model = DecisionTreeClassifier()
+model = lgb.LGBMClassifier(
+    learning_rate=0.01,
+    max_depth=7,
+    n_estimators=200,
+    num_leaves=50,
+    verbose=-1
+)
+
+# Обучение модели
 model.fit(X_train, y_train)
 
 # Сохранение модели
 joblib.dump(model, 'model.pkl')
 
+# Предсказание
 y_pred = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
+
+
+y_true = y_test['Доля 2 последний семестр'].values
+accuracy = accuracy_score(y_true, y_pred)
+
+# Оценка модели
+mae = mean_absolute_error(y_test, y_pred)
+
+r2 = r2_score(y_test, y_pred)
+
+# Сохранение предсказаний
 predictions_df = pd.DataFrame(y_pred, columns=['Predicted'])
 predictions_df.to_csv('predictions.csv', index=False)
 
-print("Точность:", accuracy)
 
+print(f"MAE: {mae:.2f}")
 
+print(f"R^2 Score: {r2:.2f}")
+
+print(f"Accuracy : {accuracy:.2f}")
